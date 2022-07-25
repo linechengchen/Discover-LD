8bded9b17040d957db62f372aca31c4c6b41b4db
cch <2480402065@qq.com>
2022-07-25 14:06:32 +0800

    Changes update dcat2.0

---
+++ database/migrations/2022_07_25_140238_create_pay_method_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePayMethodTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pay_method', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->default('')->comment('付款方式');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pay_method');
+    }
+}
